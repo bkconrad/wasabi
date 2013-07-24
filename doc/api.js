d493e1f36b528581efc0fa6e85a00530fa9080a3
Bryan Conrad <conrad.bryan.k@gmail.com>
2013-07-23 17:40:18 -0700

    add generated yui docs

---
+++ doc/api.js
@@ -0,0 +1,15 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Bitstream",
+        "Connection",
+        "InDescription",
+        "OutDescription",
+        "Registry",
+        "Rpc",
+        "Wasabi"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
