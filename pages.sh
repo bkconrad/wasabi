d0f4b333906d63daf4a1c519b5f0e05f647581ff
Bryan Conrad <bkconrad@gmail.com>
2014-01-01 12:02:42 -0800

    update pages.sh

--- pages.sh
+++ pages.sh
@@ -1,9 +1,16 @@
 jake doc
+jake test
 mv doc doc-tmp
+mv cov cov-tmp
+
 git checkout gh-pages
-rm -r doc
+
+rm -r doc cov
 mv doc-tmp doc
-git add doc
-git commit -a -m "update docs on `date`"
+mv cov-tmp cov
+
+git add doc cov
+git commit -a -m "update docs and coverage on `date`"
+
 echo 'Pages updated'
 git checkout master
