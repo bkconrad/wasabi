eb7b633a6106e0f59368e6f79bc07b4d2fbf9907
Bryan Conrad <bkconrad@gmail.com>
2014-01-04 16:23:42 -0800

    add InDescription docs for formally documenting types

--- src/in_description.js
+++ src/in_description.js
@@ -22,22 +22,85 @@ var InDescription = function (bs, target, serialize) {
 };
 
 InDescription.prototype = {
+    /**
+     * Describe an unsigned integer
+     * @param {String} name The name of the attribute
+     * @param {Number} bits The number of bits to use when encoding
+     * @method uint
+     */
     uint: function (name, bits) {
         this._bitStream.writeUInt(this._target[name], bits);
     },
 
+    /**
+     * Describe a signed integer
+     * @param {String} name The name of the attribute
+     * @param {Number} bits The number of bits to use when encoding, including
+     *     the sign bit
+     * @method sint
+     */
     sint: function (name, bits) {
         this._bitStream.writeSInt(this._target[name], bits);
     },
 
+    /**
+     * Describe a normalized float on the range [-1.0, 1.0].
+     *
+     * **Values will be clamped to this range**.
+     *
+     * @param {String} name The name of the attribute
+     * @param {Number} bits The number of bits to use when encoding, including
+     *     the sign bit
+     * @method float
+     */
     float: function (name, bits) {
         this._bitStream.writeFloat(this._target[name], bits);
     },
 
+    /**
+     * Describe a string of 8-bit characters
+     *
+     * String length is limited only by memory and bandwidth, but MUST NOT
+     * contain a null character (0x00).
+     *
+     * @param {String} name The name of the attribute
+     * @method string
+     */
     string: function (name) {
         this._bitStream.writeString(this._target[name]);
     },
 
+    /**
+     * Describes an entire subobject using an optional `serialize`-like function.
+     *
+     *     ObjectEncodingTestClass.prototype.serialize = function (desc) {
+     * 
+     *         // a property named 'structuredObj' with explicit structure
+     *         // using a supplied serialize function
+     *         desc.object('structuredObj', function (desc1) {
+     *             desc1.uint('uintfoo', 8);
+     *             desc1.sint('sintfoo', 8);
+     *
+     *             // note that you can declare nested structures
+     *             desc1.object('subobject', function (desc2) {
+     *                 desc2.uint('uintbar', 8);
+     *                 desc2.sint('sintbar', 8);
+     *             });
+     *         });
+     * 
+     *         // if no serialize function is supplied, Wasabi will recursively
+     *         // descend into the object and encode it using InDescription#any
+     *         desc.object('unstructuredObj');
+     *
+     *     };
+     *
+     * **Note:** Encoding an object with cyclical references is not supported,
+     * and will currently cause Wasabi to recurse infinitely 
+     *
+     * @param {String} name The name of the attribute
+     * @param {Function} serialize Optional serialize function to use
+     * @method string
+     */
     object: function (name, serialize) {
         var obj = this._target[name] || {};
 
@@ -63,6 +126,22 @@ InDescription.prototype = {
         this._bitStream.pack(obj, serialize);
     },
 
+    /**
+     * Describe a property of unknown type
+     *
+     * Wasabi will deduce and encode the variable's type, if it is supported,
+     * throwing an error if it is of an unsupported type (null, undefined,
+     * Array, and Function, at present)
+     *
+     * **Note:** Data encoded with this method takes up 3 more bits than data
+     * encoded with an explicit type, and requires more CPU on the server for
+     * type deduction.
+     *
+     * @param {String} name The name of the attribute
+     * @param {Number} bits The number of bits used to encode the data (if
+     *     applicable)
+     * @method string
+     */
     any: function (name, bits) {
         var type;
         var val = this._target[name];
